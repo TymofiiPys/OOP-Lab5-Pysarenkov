d922048c53103fd219f97f709aa53a7a80316f43
TymofiiPys <nfstimoha2004@gmail.com>
2024-05-13 17:08:00 +0300

    Init commit (i see that what has been here before the commit is not needed anymore)

---
+++ src/main/java/com/restaurant/OopLab5PysarenkovApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopLab5PysarenkovApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopLab5PysarenkovApplication.class, args);
+    }
+
+}
