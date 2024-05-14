1c130d9523ca7d0d7b3587a4aa889687dae8b41d
TymofiiPys <nfstimoha2004@gmail.com>
2024-05-14 13:41:12 +0300

    Enable repo and change port

--- src/main/java/com/restaurant/RestaurantApplication.java
+++ src/main/java/com/restaurant/RestaurantApplication.java
@@ -2,8 +2,10 @@ package com.restaurant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.restaurant.repository")
 public class RestaurantApplication {
 
     public static void main(String[] args) {
