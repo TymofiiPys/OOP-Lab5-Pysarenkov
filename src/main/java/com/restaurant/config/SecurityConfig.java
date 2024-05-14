192c8c8ff62c1bd705f21b8d67adc35a5e5d4e5f
TymofiiPys <nfstimoha2004@gmail.com>
2024-05-14 17:00:26 +0300

    Add filtering

--- src/main/java/com/restaurant/config/SecurityConfig.java
+++ src/main/java/com/restaurant/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.restaurant.config;
 
+import com.restaurant.filter.JWTFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -20,7 +23,8 @@ public class SecurityConfig {
 //                .authorizeHttpRequests()
 //                .anyRequest().authenticated();
 
-        http.csrf().disable();
+        http.csrf(AbstractHttpConfigurer::disable);
+//        http.addFilter(new JWTFilter());
         return http.build();
 
 //        http
@@ -36,5 +40,13 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public FilterRegistrationBean<JWTFilter> JWTFilter() {
+        FilterRegistrationBean<JWTFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new JWTFilter());
+        registrationBean.addUrlPatterns("*");
+        return registrationBean;
     }
 }
---
+++ src/main/java/com/restaurant/filter/JWTFilter.java
@@ -0,0 +1,55 @@
+package com.restaurant.filter;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.restaurant.entity.Client;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
+public class JWTFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        String token = request.getHeader("access-token");
+        Algorithm algorithm = Algorithm.HMAC256("secretlysecret");
+        JWTVerifier verifier = JWT.require(algorithm)
+                .withIssuer("IMBARESTAURANT")
+                .build();
+        DecodedJWT decodedJWT = verifier.verify(token);
+        if (decodedJWT.getExpiresAt().before(new Date())) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+        Long id = decodedJWT.getClaim("id").asLong();
+        String email = decodedJWT.getClaim("email").asString();
+        boolean isAdmin = decodedJWT.getClaim("isAdmin").asBoolean();
+        Client client = new Client();
+        client.setId(id);
+        client.setEmail(email);
+        client.setAdmin(isAdmin);
+        request.setAttribute("client", client);
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        String path = request.getRequestURI();
+        return "/auth".equals(path);
+    }
+}
